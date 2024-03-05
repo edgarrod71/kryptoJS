76b94d5235d2a74ff9b4589572f556ad5b2c9b39
Edgar Fernando <edgarrod71@gmail.com>
2024-03-05 18:59:32 -0500

    intentando actualizar

--- encriptador.js
+++ encriptador.js
@@ -1,31 +1,37 @@
-let frase = "";
-let copiada = "";
-let cambios = {
-    'e': 'enter',
-    'i': 'imes',
-    'a': 'ai',
-    'o': 'ober',
-    'u': 'ufat'
-};
-
-function encriptar(selector) {
-    var contenido = document.querySelector(selector);
-    let frase = contenido.textContent;
-
-    return frase;
+var copiada = "";
+let vocales = ['a',     'e',        'i',    'o',    'u'];
+let valores = ['ai',    'enter',    'imes', 'ober', 'ufat'];
+let enumED = {
+    encripte: 1,
+    desencripte: 0
 }
 
-function desencriptar() {
-    return frase;
+function krypto(selector, destino, tipo) {
+    var contenido = document.querySelector(selector);
+    var traducido = document.querySelector(destino);
+    let strMay = contenido.value;
+    if (tipo === enumED.encripte) 
+        for (let i=0; i < vocales.length; i++) {
+            strMay = strMay.replaceAll(vocales[i], valores[i].toUpperCase());
+        }
+    else 
+        for (let i=0; i < vocales.length; i++) {
+            strMay = strMay.replaceAll(valores[i], vocales[i].toUpperCase());
+        }
+    traducido.textContent = strMay.toLowerCase();    
 }
 
 function copiar(selector, destino) {
-    var textArea = document.querySelector(destino);
-    textArea.select();
+    var textDestino = document.querySelector(destino);
+    var textSelector = document.querySelector(selector);
+    textDestino.select();
     document.execCommand("copy");
-    textArea.blur();
-    copiada = textArea.textContent;
-    // copiada = textArea.textContent;
-    // var textDestino = document.querySelector(destino);
-    // textDestino.innerHTML = textArea.textContent;
-}
+    copiada = textDestino.value;
+    textDestino.blur();
+    textSelector.value = '';
+    textSelector.value = copiada;
+    console.log(copiada);
+    textSelector.select();
+    textDestino.blur();
+    textSelector.blur();
+  }
