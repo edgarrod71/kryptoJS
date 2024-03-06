0b676b2c58d45807f9d4e02ac70186c19c6f765f
Edgar Fernando <edgarrod71@gmail.com>
2024-03-06 05:39:24 -0500

    actualizando el challenge

--- encriptador.js
+++ encriptador.js
@@ -5,11 +5,12 @@ let enumED = {
     encripte: 1,
     desencripte: 0
 }
-
+let mError = "El texto tiene mayúsculas, esto no es permitido y se tratará como minúsculas ";
 function krypto(selector, destino, tipo) {
     var contenido = document.querySelector(selector);
     var traducido = document.querySelector(destino);
     let strMay = contenido.value;
+    //if (!/[A-Z]/.test(strMay)) alert(mError);
     if (tipo === enumED.encripte) {
         for (let i=0; i < vocales.length; i++) {
             strMay = strMay.replaceAll(vocales[i], valores[i].toUpperCase());
